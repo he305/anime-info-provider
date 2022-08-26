3b3efff0ae4572695ad45a566a96a58318133911
sergey <he305@mail.ru>
2022-08-26 16:52:50 +0300

    Initial commit

---
+++ src/main/java/com/github/he305/animeinfoprovider/AnimeInfoProviderApplication.java
@@ -0,0 +1,13 @@
+package com.github.he305.animeinfoprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeInfoProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeInfoProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/he305/animeinfoprovider/AnimeInfoProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.he305.animeinfoprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeInfoProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
